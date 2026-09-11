d3a1b4f4f907d6095a6fc8ab75ebe47fc53581e1
Marius <aceteaxh@gmail.com>
2026-09-11 09:08:13 +0800

    upd

--- my_flutter_app/lib/screens/profile_screen.dart
+++ my_flutter_app/lib/screens/profile_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:intl/intl.dart';
 import '../constants.dart';
 import '../widgets/custom_font.dart';
 import '../widgets/custom_button.dart';
--- my_flutter_app/lib/services/post_service.dart
+++ my_flutter_app/lib/services/post_service.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 import 'package:http/http.dart';
-
-import '../constants.dart';
 import '../models/post.dart';
 
 class PostService {
